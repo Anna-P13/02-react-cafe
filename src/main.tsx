589601833cbeb88559c1e62c5d973a04bb8d625d
Anna Pastykhova <rainbow261@gmail.com>
2025-08-08 12:37:58 +0300

    Revert "hw"
    This reverts commit 0cd0f8ab1ebdb921cfea9c0760117ec9edaec918.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "modern-normalize";
-import "./index.css";
-import App from "./components/App/App.tsx";
 
-createRoot(document.getElementById("root") as HTMLDivElement).render(
-  <StrictMode>
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "modern-normalize";
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
